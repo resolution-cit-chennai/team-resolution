d34c2d6ba12094ae7015fc4c126f864a9acc416a
kalai-mugilan-125 <mugilankalai.123@gmail.com>
2026-09-11 18:24:45 +0530

    feat: replace header logo with logo.svg

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,5 +1,5 @@
 import { useEffect, useState } from "react";
-import logo from "../assets/logo.png";
+import logo from "../assets/logo.svg";
 import { Sparkles, ArrowRight } from "lucide-react";
 
 export default function Nav() {
